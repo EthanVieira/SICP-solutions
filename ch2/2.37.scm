6c7ad3ce750d68be35a8f3bed8f1f128fc09678f
EthanVieira <ethanvieira@gmail.com>
2018-12-26 15:27:48 -0600

    add 2.38

--- ch2/2.37.scm
+++ ch2/2.37.scm
@@ -2,6 +2,12 @@
 
 (define nil '())
 
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       nil
